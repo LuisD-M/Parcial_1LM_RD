893aa2b7beb4bc5c53045d670c6fff820eb7af25
raul-daza <raul.daza@udea.edu.co>
2023-09-24 20:08:28 -0500

    "codigo comentado"

--- Codigo_Arduino/Codigo_Arduino.ino
+++ Codigo_Arduino/Codigo_Arduino.ino
@@ -1,25 +1,32 @@
-int pinData  = 2;
-int pinLatch = 3;
-int pinClock = 4;
+int pinData  = 2; // pin por donde se enviara la informacion de forma serial al H595
+int pinLatch = 3; // pin que define cuando se desplegara la informacion en paralelo en el H595
+int pinClock = 4; // pin que define cuando se ingresa un nuevo bit al H595
 
 
 void setup(){
+  // velocidad de baudios de la comunicacion serial
   Serial.begin(9600);
+  // declaracion de los pines de comunicacion con el H595 como salida
   pinMode(pinData, OUTPUT);
   pinMode(pinLatch, OUTPUT);
   pinMode(pinClock, OUTPUT);
 }
 
 void loop() {
-  int opcion=1, repeticiones=0;
-  long tiempo=0;
+  int opcion=1; // opcion es la opcion escogida en el menu por el usuario
+  int repeticiones=0; // repeticiones se usa en algunas funciones para repetir una imagen el la matriz de leds 
+  long tiempo=0; // guarda el tiempo que se desplegara cada imagen en la matriz de leds
 
+  // creacion de la matriz que guardara las imagenes a desplegar en la matriz de leds
   int **matriz = new int*[8];                       
   for(int i=0; i<8; i++) matriz[i] = new int[8];
 
+  // bucle principal del programa
   while(opcion!=4){
   
+    // llama al menu
     opcion = Public();
+    // segun lo escogido por el usuario se va al caso correspondiente
     switch (opcion)
     {
       case 1:
@@ -85,6 +92,7 @@ void loop() {
   while(opcion == 4){}
 }
 
+// despliega el menu y retorna la opcion escogida por el usuario
 int Public(){
   int opcion=10;
   Serial.println(" --------- Menu de opciones ---------- ");
@@ -102,6 +110,8 @@ int Public(){
   return opcion;
 }
 
+
+// apaga la matriz de leds
 void apagar(int **matriz){
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++) matriz[i][j] = 0;     //Llena la matriz con ceros
@@ -109,6 +119,7 @@ void apagar(int **matriz){
     H595(0,0);
 }
 
+// enciende la matriz de leds
 void prender(int **matriz){
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++) matriz[i][j] = 1;     //Llena la matriz con ceros
@@ -116,6 +127,7 @@ void prender(int **matriz){
     H595(255,0);
 }
 
+// hace la funcion de verificar que la matriz de leds esta funcionando, consiste en apagar y encender todos los leds de la matriz
 void verificacion(int **matriz, long tiempo, int repeticiones){
   for(int i=0; i<repeticiones; i++){
       prender(matriz);
@@ -125,6 +137,7 @@ void verificacion(int **matriz, long tiempo, int repeticiones){
   }
 }
 
+// recibe por parte del usuario que leds se van a encender o apagar y luego despliega la imagen ingresada por el usuario
 void imagen(int **matriz, long tiempo){
   for(int i=0; i<8; i++){                                           //Llena la funcion.
       for(int j=0; j<8; j++){
@@ -145,6 +158,7 @@ void imagen(int **matriz, long tiempo){
   H595(0,0);
 }
 
+// llena la matriz con los '0's y '1's necesarios para mostrar la imagen del patron 1 
 void patron1(int **matriz){
   int aux=8;
 
@@ -164,6 +178,7 @@ void patron1(int **matriz){
   }
 }
 
+// llena la matriz con los '0's y '1's necesarios para mostrar la imagen del patron 2
 void patron2(int **matriz){
   for(int i=0; i<8; i++){
       for(int j=0; j<8; j++ ){
@@ -175,6 +190,7 @@ void patron2(int **matriz){
   for(int i=0; i<8; i++) matriz[i][7-i]=1;
 }
 
+// llena la matriz con los '0's y '1's necesarios para mostrar la imagen del patron 3
 void patron3(int **matriz){
   int contador=0, b=0;
 
@@ -209,6 +225,7 @@ void patron3(int **matriz){
   }
 }
 
+// llena la matriz con los '0's y '1's necesarios para mostrar la imagen del patron 4
 void patron4(int **matriz){
 
   for(int i=0; i<4; i++){                                          //Primeras 4 filas.
@@ -224,6 +241,7 @@ void patron4(int **matriz){
   }
 }
 
+// despliega alternadamente los patrones en la matriz de leds
 void  patrones(int **matriz, long tiempo){
 
   patron1(matriz);
@@ -246,6 +264,7 @@ void  patrones(int **matriz, long tiempo){
 
 }
 
+// traduce la matriz de '1's y '0's en los bits necesarios para desplegar la imagen (fila por fila) y luego los envia a los integrados H595
 void vector2sum(int **matriz, long tiempo){
   byte sumRow=0;
   byte potencias[8]={1,2,4,8,16,32,64,128};
@@ -263,6 +282,7 @@ void vector2sum(int **matriz, long tiempo){
   }
 }
 
+// funcion de control del integrado H595, envia los datos de forma serial a dos integrados en serie y luego los activa
 void H595(byte an, byte cat ){
    shiftOut(pinData, pinClock, MSBFIRST, (byte)an);
    shiftOut(pinData, pinClock, MSBFIRST, (byte)cat);
